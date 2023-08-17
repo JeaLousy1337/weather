aaf54af24825963ea8063beeeb4f87d2a46c9fff
JeaLousy1337 <timurka.t.r@gmail.com>
2023-08-18 00:27:30 +0300

    fixed

--- weather.py
+++ weather.py
@@ -1,10 +1,19 @@
+from pywebio.input import input as pw_input
+from pywebio.output import put_success, put_warning, toast
 import pyowm
-city = (input("Введите город:"))
-ip = pyowm.OWM('9a841d6c2d67f9309cb359f37e051f6b')
-mgr = ip.weather_manager()
-observation = mgr.weather_at_place(city)
-w = observation.weather
-temp = w.temperature('celsius')["temp"]
-all_funk = f'городе {city} сейчас {temp} градусов'
-print(all_funk)
-input()
+
+weather = True
+
+while weather is True:
+    if weather is False:
+        break
+    city = pw_input("Введите свой город:")
+    owm = pyowm.OWM('9a841d6c2d67f9309cb359f37e051f6b')
+    mgr = owm.weather_manager()
+    observation = mgr.weather_at_place(city)
+    w = observation.weather
+    temp = w.temperature('celsius')["temp"]
+    if temp > 20:
+        put_success(f'В городе {city} сейчас {temp} градусов, можно одеваться достаточно легко \U0001F305')
+    else:
+        put_warning(f'В городе {city} сейчас {temp} градусов, достаточно холодно, одевайся потеплее \U0001F976')
